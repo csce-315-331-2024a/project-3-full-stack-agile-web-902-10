fdab12243052ade3affcdfd1a084b0932c419cb2
zsmeikle <zmeikle@tamu.edu>
2024-03-31 18:47:22 -0500

    Revert "Tried to do more but fell short, did fix bugs"
    This reverts commit da4a538608c325a4f73faaad386fa432e281bcf0.

--- components/MenuBoardClient.tsx
+++ components/MenuBoardClient.tsx
@@ -14,131 +14,50 @@ export default function MenuBoardClient({ menu_items, categories}:
     }) {
     
     const getMenuItems = (cat: string) => {
-        let menuItems = [""];
+        let menuItems = [": "];
         for (let i = 0; i < menu_items.length; ++i){
             if (menu_items[i].category == cat){
-                menuItems.push(" | " + menu_items[i].name + ": $"+menu_items[i].price.toString() + " | ");
+                menuItems.push(menu_items[i].name);
+                menuItems.push(" ");
+                menuItems.push("$"+menu_items[i].price.toString());
+                menuItems.push(" | ");
             }
         }
-        menuItems.splice(0,1);
         return menuItems;
     }
 
-    /*const firstFive = (strAr: string[]) => {
-        let outAr = [""];
-        for (let i = 0; i < 5; ++i){
-            if(i < strAr.length){
-                outAr.push(strAr[i]);
-            }
-        }
-        outAr.splice(0,1);
-        return outAr;
-    }
-    const removeFirstFive = (strAr: string[]) => {
-        strAr.splice(0,5)
-        return strAr;
-    }*/
-    
-
 
     const [index, setIndex] = useState(0);
-    //const [count, setCount] = useState(0);
-
-    //let preCat1 = categories[0];
-    //let preMenuItems1 = getMenuItems(categories[0]);
-
-
-    //const [preMenuItems1, spreMenuItems1] = useState(getMenuItems(categories[0]));
-
-
-    //let preCat2 = categories[1];
-    //let preMenuItems2 = getMenuItems(categories[1]);
-
-
-    //const [preMenuItems2, spreMenuItems2] = useState(getMenuItems(categories[1]));
-
-    const [currentCategory1, setCurrentCat1] = useState(categories[0]);
-    const [currentMenuItems1, setCurrentMI1] = useState(getMenuItems(categories[0]));//useState(firstFive(preMenuItems1));
+    const [currentCategory, setCurrentCat] = useState(categories[0]);
+    const [currentMenuItems, setCurrentMI] = useState(getMenuItems(categories[0]));
 
     const [currentCategory2, setCurrentCat2] = useState(categories[1]);
-    const [currentMenuItems2, setCurrentMI2] = useState(getMenuItems(categories[1]));//useState(firstFive(preMenuItems2));
-
+    const [currentMenuItems2, setCurrentMI2] = useState(getMenuItems(categories[1]));
 
 
 
     useEffect(() => {
-        const switchString = () => {
-            /*if (preMenuItems1.length <=5 && preMenuItems2.length <=5){
-                setCount(count + 1);
-                const next = (index + 2);
-                setIndex(next%categories.length);
+    const switchString = () => {
+        const next = (index + 1) % categories.length;
+        setIndex(next);
+        setCurrentCat(categories[next]);
+        setCurrentMI(getMenuItems(categories[next]));
+        setCurrentCat2(categories[next+1]);
+        setCurrentMI2(getMenuItems(categories[next+1]));
+    };
 
-                setCurrentCat1(categories[(next-1)%categories.length]);
-                spreMenuItems1(getMenuItems(currentCategory1));
-                setCurrentMI1(firstFive(preMenuItems1));
+    const intervalId = setInterval(switchString, 5000); 
 
-                setCurrentCat2(categories[next%categories.length]);
-                spreMenuItems2(getMenuItems(currentCategory2));
-                setCurrentMI2(firstFive(preMenuItems2));
-            }
-            else if (preMenuItems1.length >5 && preMenuItems2.length >5){
-                setCount(count + 1);
-                //setIndex(index);
-                //setCurrentCat1(categories[(index-1)%categories.length])
-                spreMenuItems1(removeFirstFive(preMenuItems1));
-                setCurrentMI1(firstFive(preMenuItems1));
-                //setCurrentCat2(categories[index%categories.length]);
-                spreMenuItems2(removeFirstFive(preMenuItems2));
-                setCurrentMI2(firstFive(preMenuItems2));
-
-                //setCurrentCat1(categories[(index-1)%categories.length]);
-                //setCurrentCat2(categories[(index)%categories.length]);
-            }
-            else if (preMenuItems1.length > 5 && preMenuItems2.length <= 5){
-                setCount(count + 1);
-                //setIndex(index);
-                spreMenuItems1(removeFirstFive(preMenuItems1));
-                setCurrentMI1(firstFive(preMenuItems1));
-                //setCurrentCat1(categories[(index-1)%categories.length]);
-                //setCurrentCat2(categories[index%categories.length]);
-                setCurrentMI2(currentMenuItems2);
-                //setCurrentCat1(categories[(index-1)%categories.length]);
-                //setCurrentCat2(categories[(index)%categories.length]);
-            }
-            else{//if 1 is below and 2 is over
-                setCount(count + 1);
-                //setIndex(index);
-                spreMenuItems2(removeFirstFive(preMenuItems2));
-                setCurrentMI2(firstFive(preMenuItems2));
-                //setCurrentCat1(categories[(index-1)%categories.length]);
-                //setCurrentCat2(categories[index%categories.length]);
-                //setCurrentMI1(currentMenuItems1);
-                //setCurrentCat1(categories[(index-1)%categories.length]);
-                //setCurrentCat2(categories[(index)%categories.length]);
-            }*/
-
-
-            const next = (index + 2) ;
-            setIndex(next% categories.length);
-            setCurrentCat1(categories[(next-1)% categories.length]);
-            setCurrentMI1(getMenuItems(categories[(next-1)% categories.length]));
-            setCurrentCat2(categories[(next)% categories.length]);
-            setCurrentMI2(getMenuItems(categories[(next)% categories.length]));
-        };
-
-        const intervalId = setInterval(switchString, 5000); 
-
-        return () => clearInterval(intervalId);
-    }, [index]);//, count, currentCategory1, currentCategory2, currentMenuItems1, currentCategory2]); 
+    return () => clearInterval(intervalId);
+    }, [index]); 
     
     
     return (
         <div>
         
         
-        <p> {currentCategory1}  {currentMenuItems1}</p>
+        <p> {currentCategory}  {currentMenuItems}</p>
         <p> {currentCategory2}  {currentMenuItems2}</p>
-        //<p> {index}</p>
         
         
         </div>
