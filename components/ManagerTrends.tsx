1f5d213f31b35fa0e10daaf403ad254bdbc5da71
anuccitelli2 <anuccitelli2@tamu.edu>
2024-03-31 00:44:55 -0500

    Added framework for a basic datatable with filler data
    Added the table.tsx file, which contains the code to make a datatable, and implemented a test table in ManagerTrends

--- components/ManagerTrends.tsx
+++ components/ManagerTrends.tsx
@@ -3,6 +3,15 @@
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
 import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table"
 
 export default function ManagerTrends() {
 
@@ -16,6 +25,27 @@ export default function ManagerTrends() {
                     
                 </div>
                 <ScrollBar orientation="vertical" />
+            </ScrollArea>
+            <ScrollArea className="h-[92vh] w-[80vw] p-8 whitespace-nowrap">
+                <Table>
+                    <TableCaption>A list of your recent invoices.</TableCaption>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead className="w-[100px]">Invoice</TableHead>
+                            <TableHead>Status</TableHead>
+                            <TableHead>Method</TableHead>
+                            <TableHead className="text-right">Amount</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        <TableRow>
+                        <TableCell className="font-medium">INV001</TableCell>
+                        <TableCell>Paid</TableCell>
+                        <TableCell>Credit Card</TableCell>
+                        <TableCell className="text-right">$250.00</TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
             </ScrollArea>
         </div>
     );
