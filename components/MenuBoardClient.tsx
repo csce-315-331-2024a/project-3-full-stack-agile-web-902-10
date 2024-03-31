ecc27856563a724e0ecb6cf05e1b4eab041eb91f
zsmeikle <zmeikle@tamu.edu>
2024-03-31 18:46:48 -0500

    Revert "Cleaned up phase 1"
    This reverts commit 8129b4243d4b3e3e08d992513f7eb3e55897b29b.

--- components/MenuBoardClient.tsx
+++ components/MenuBoardClient.tsx
@@ -13,7 +13,6 @@ export default function MenuBoardClient({ menu_items, categories}:
         categories: string[]
     }) {
     
-    //Gets menu items in a category
     const getMenuItems = (cat: string) => {
         let menuItems = [""];
         for (let i = 0; i < menu_items.length; ++i){
@@ -25,8 +24,20 @@ export default function MenuBoardClient({ menu_items, categories}:
         return menuItems;
     }
 
-
-    //definitions
+    /*const firstFive = (strAr: string[]) => {
+        let outAr = [""];
+        for (let i = 0; i < 5; ++i){
+            if(i < strAr.length){
+                outAr.push(strAr[i]);
+            }
+        }
+        outAr.splice(0,1);
+        return outAr;
+    }
+    const removeFirstFive = (strAr: string[]) => {
+        strAr.splice(0,5)
+        return strAr;
+    }*/
     let preMenu1 = [[""]];
     preMenu1.splice(0,1);
     let temp1 = [""];
@@ -43,7 +54,11 @@ export default function MenuBoardClient({ menu_items, categories}:
     preCategories2.splice(0,1);
 
 
-    //gets all the elements for the first half of the menu board
+    /*let temp11 = [""];
+    temp11.splice(0,1);
+
+    let temp21 = [""];
+    temp21.splice(0,1);*/
     for (let i = 0; i < categories.length; i = i +2){
         temp1 = getMenuItems(categories[i]);
         while (temp1.length > 5){  
@@ -53,7 +68,6 @@ export default function MenuBoardClient({ menu_items, categories}:
         preMenu1.push(temp1);
         preCategories1.push(categories[i]);
      }
-     //gets all the elements for the second half of the menu board
      for (let i = 1; i < categories.length; i = i +2){
         temp2 = getMenuItems(categories[i]);
         while (temp2.length > 5){  
@@ -64,24 +78,139 @@ export default function MenuBoardClient({ menu_items, categories}:
         preCategories2.push(categories[i]);
      }
 
+    /*for (let i = 0; i < categories.length; i = i + 2){
+        if (i  == categories.length - 1){
+            
+            temp1 = getMenuItems(categories[i]);
+            while (temp1.length > 5){
+                //temp11 = temp1.splice(0,5);
+                //preMenu1.push(temp11);
+                preMenu1.push(temp1.splice(0,5));
+                preCategories1.push(categories[i]);
+                preMenu2.push(temp2);
+                preCategories2.push(categories[i-1]);
+            }
+            preMenu1.push(temp1);
+            preCategories1.push(categories[i]);
+            preMenu2.push(temp2);
+            preCategories2.push(categories[i-1]);
+        }
+        else {
+            preCategories1.push(categories[i]);
+            preCategories2.push(categories[i+1]);
+
+            temp1 = getMenuItems(categories[i]);
+            temp2 = getMenuItems(categories[i+1]);
+
+            while(temp1.length > 5 || temp2.length > 5){
+                if (temp1.length > 5){
+                    //temp11 = temp1.splice(0,5);
+                    //preMenu1.push(temp11);
+                    preMenu1.push(temp1.splice(0,5));
+                    preCategories1.push(categories[i]);
+                }
+                else {
+                    preMenu1.push(temp1);
+                    preCategories1.push(categories[i]);
+                }
+
+                if (temp2.length > 5){
+                    //temp21 = temp2.splice(0,5);
+                    //preMenu2.push(temp21);
+                    preMenu2.push(temp2.splice(0,5));
+                    preCategories2.push(categories[i+1]);
+                }
+                else {
+                    preMenu2.push(temp2);
+                    preCategories2.push(categories[i+1]);
+                }
+            }
+            preMenu1.push(temp1);
+            preCategories1.push(categories[i]);
+
+            preMenu2.push(temp2);
+            preCategories2.push(categories[i+1]);
+        }
+    }*/
     
 
 
-     //states that are changing
     const [index1, setIndex1] = useState(0);
     const [index2, setIndex2] = useState(0);
+    //const [count, setCount] = useState(0);
+
+    //let preCat1 = categories[0];
+    //let preMenuItems1 = getMenuItems(categories[0]);
+
+
+    //const [preMenuItems1, spreMenuItems1] = useState(getMenuItems(categories[0]));
+
+
+    //let preCat2 = categories[1];
+    //let preMenuItems2 = getMenuItems(categories[1]);
+
+
+    //const [preMenuItems2, spreMenuItems2] = useState(getMenuItems(categories[1]));
 
     const [currentCategory1, setCurrentCat1] = useState(preCategories1[0]);
-    const [currentMenuItems1, setCurrentMI1] = useState(preMenu1[0]);
+    const [currentMenuItems1, setCurrentMI1] = useState(preMenu1[0]);//useState(firstFive(preMenuItems1));
 
     const [currentCategory2, setCurrentCat2] = useState(preCategories2[0]);
-    const [currentMenuItems2, setCurrentMI2] = useState(preMenu2[0]);
+    const [currentMenuItems2, setCurrentMI2] = useState(preMenu2[0]);//useState(firstFive(preMenuItems2));
 
 
 
-    //function that updates the menu board
+
     useEffect(() => {
         const scroll = () => {
+            /*if (preMenuItems1.length <=5 && preMenuItems2.length <=5){
+                setCount(count + 1);
+                const next = (index + 2);
+                setIndex(next%categories.length);
+
+                setCurrentCat1(categories[(next-1)%categories.length]);
+                spreMenuItems1(getMenuItems(currentCategory1));
+                setCurrentMI1(firstFive(preMenuItems1));
+
+                setCurrentCat2(categories[next%categories.length]);
+                spreMenuItems2(getMenuItems(currentCategory2));
+                setCurrentMI2(firstFive(preMenuItems2));
+            }
+            else if (preMenuItems1.length >5 && preMenuItems2.length >5){
+                setCount(count + 1);
+                //setIndex(index);
+                //setCurrentCat1(categories[(index-1)%categories.length])
+                spreMenuItems1(removeFirstFive(preMenuItems1));
+                setCurrentMI1(firstFive(preMenuItems1));
+                //setCurrentCat2(categories[index%categories.length]);
+                spreMenuItems2(removeFirstFive(preMenuItems2));
+                setCurrentMI2(firstFive(preMenuItems2));
+
+                //setCurrentCat1(categories[(index-1)%categories.length]);
+                //setCurrentCat2(categories[(index)%categories.length]);
+            }
+            else if (preMenuItems1.length > 5 && preMenuItems2.length <= 5){
+                setCount(count + 1);
+                //setIndex(index);
+                spreMenuItems1(removeFirstFive(preMenuItems1));
+                setCurrentMI1(firstFive(preMenuItems1));
+                //setCurrentCat1(categories[(index-1)%categories.length]);
+                //setCurrentCat2(categories[index%categories.length]);
+                setCurrentMI2(currentMenuItems2);
+                //setCurrentCat1(categories[(index-1)%categories.length]);
+                //setCurrentCat2(categories[(index)%categories.length]);
+            }
+            else{//if 1 is below and 2 is over
+                setCount(count + 1);
+                //setIndex(index);
+                spreMenuItems2(removeFirstFive(preMenuItems2));
+                setCurrentMI2(firstFive(preMenuItems2));
+                //setCurrentCat1(categories[(index-1)%categories.length]);
+                //setCurrentCat2(categories[index%categories.length]);
+                //setCurrentMI1(currentMenuItems1);
+                //setCurrentCat1(categories[(index-1)%categories.length]);
+                //setCurrentCat2(categories[(index)%categories.length]);
+            }*/
             let next1 = (index1 + 1)%preCategories1.length ;
             let next2 = (index2 + 1)%preCategories2.length ;
 
@@ -104,15 +233,16 @@ export default function MenuBoardClient({ menu_items, categories}:
         const intervalId = setInterval(scroll, 5000); 
 
         return () => clearInterval(intervalId);
-    }, [index1, index2]);
+    }, [index1, index2]);//, count, currentCategory1, currentCategory2, currentMenuItems1, currentCategory2]); 
     
-    //output
+    
     return (
         <div>
         
         
         <p> {currentCategory1}  {currentMenuItems1}</p>
         <p> {currentCategory2}  {currentMenuItems2}</p>
+        <p> {preCategories1.length} {preCategories2.length} {preMenu1.length} {preMenu2.length} {index1} {index2}</p>
         
         
         </div>
