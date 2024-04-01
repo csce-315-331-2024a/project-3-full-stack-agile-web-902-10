f91c651f88367312816984bc79dee40fd32a110b
zsmeikle <zmeikle@tamu.edu>
2024-03-31 19:04:04 -0500

    Revert "Revert "Pulling data from database""
    This reverts commit 2830a41066fb29ed2211e24b93ceb2411227cf37.

--- app/menu_board/page.tsx
+++ app/menu_board/page.tsx
@@ -1,5 +1,18 @@
+import { prisma } from "@/lib/db";
+import { Menu_Item } from "@prisma/client";
 
-export default function menu_board() {
-    return <p> Not implemented ;C</p>;
+
+export default async function menu_board() {
+    const menu_items = await prisma.menu_Item.findMany();
+    const categories = Array.from(new Set(menu_items.map((item) => item.category)));
+    
+    
+    return (
+    <div> {/*Not implemented ;C*/}
+        {menu_items.filter((menu_item) => menu_item.name === "Bacon Cheeseburger").map((menu_item) => (
+            <h2 className="text-xl text-wrap text-left">{menu_item.category}</h2>
+        ))}
+    </div>
+    );
 }
 
