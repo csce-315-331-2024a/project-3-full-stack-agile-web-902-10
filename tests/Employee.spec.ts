949a529a2649253402afad4f42228588d31e3553
wu <zhongyouwu@tamu.edu>
2024-03-31 23:12:34 -0500

    Added tests fr this time

--- tests/Employee.spec.ts
+++ tests/Employee.spec.ts
@@ -1,5 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('add items to cart and clear cart', async ({ page }) => {
-    
+    await page.goto('http://localhost:3000/manager');
+    await page.getByRole('button', { name: 'Employees' }).click();
+    await page.getByRole('button', { name: 'Zhongyou Wu Manager' }).click();
 });
