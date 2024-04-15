09821f01fd57fdc94e9c32a6fbd672774e6463f8
Sean Lee <99452617+sean3-lee@users.noreply.github.com>
2024-04-14 21:41:23 -0500

    Update CashierTests.spec.ts

--- tests/CashierTests.spec.ts
+++ tests/CashierTests.spec.ts
@@ -10,6 +10,10 @@ test('add items in cashier page', async ({ page }) => {
     await page.getByRole('button', { name: 'Beverages' }).click();
     await page.getByRole('button', { name: '20 OZ Fountain Drink' }).click();
     await page.getByRole('button', { name: 'Add to Cart' }).click();
+    await page.getByRole('button', { name: 'Classic Hamburger' }).click();
+    await page.getByRole('button', { name: 'Add to Cart' }).click();
+    await page.getByRole('button', { name: '2 Corn Dog Value Meal' }).click();
+    await page.getByRole('button', { name: 'Add to Cart' }).click();
     
     
 })
