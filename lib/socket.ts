dce10f143f4e96f79a2203facd598ec8f95d0cdb
Utsawb Lamichhane <utsawblamichhane@gmail.com>
2024-04-17 15:42:16 -0500

    Added GCP IP for hosted websocket server

--- lib/socket.ts
+++ lib/socket.ts
@@ -7,7 +7,7 @@ export function useSocket() {
     const [socket, setSocket] = useState(null)
 
     useEffect(() => {
-        const socketIo: any = io("ws://localhost:5000")
+        const socketIo: any = io("ws://34.41.223.189:5000")
 
         setSocket(socketIo)
 
