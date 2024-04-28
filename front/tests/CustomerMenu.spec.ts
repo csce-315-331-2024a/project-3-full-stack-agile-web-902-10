791d4f685f5c8b573373d69e2e307f422b03cc31
wu <zhongyouwu@tamu.edu>
2024-04-28 17:58:45 -0500

    Added category testing

--- front/tests/CustomerMenu.spec.ts
+++ front/tests/CustomerMenu.spec.ts
@@ -21,6 +21,33 @@ test('normal-checkout-test', async ({ page }) => {
     await page.getByRole('button', { name: 'Add to Cart' }).click();
     await page.getByText('Cart:').click();
     await page.getByRole('link', { name: 'Checkout' }).click();
-    await page.getByRole('button', { name: 'Pay' }).click();
-    await page.getByRole('button', { name: 'Pay' }).click();
+    await page.getByRole('button', { name: 'Pay' }).isVisible();
+  });
+
+  test('category-test', async ({ page }) => {
+    await page.goto('http://localhost:3000/menu');
+    await page.getByRole('button', { name: 'Value Meals' }).click();
+    await expect(page.getByRole('button', { name: 'Value Meals' })).toHaveCSS(
+        "background-color",
+        "rgba(22, 163, 74, 0.9)"
+    );
+    await page.getByRole('button', { name: 'Chicken' }).click();
+    await expect(page.getByRole('button', { name: 'Chicken', exact: true})).toHaveCSS(
+        "background-color",
+        "rgba(22, 163, 74, 0.9)"
+    );
+    await expect(page.getByRole('button', { name: 'Value Meals' })).toHaveCSS(
+        "background-color",
+        "rgb(244, 244, 245)"
+    );
+    await page.getByRole('button', { name: 'Burgers' }).click();
+    await expect(page.getByRole('button', { name: 'Burgers' })).toHaveCSS(
+        "background-color",
+        "rgba(22, 163, 74, 0.9)"
+    );
+    await page.getByRole('button', { name: 'Burgers' }).click();
+    await expect(page.getByRole('button', { name: 'Burgers' })).toHaveCSS(
+        "background-color",
+        "rgba(244, 244, 245, 0.8)"
+    );
   });
