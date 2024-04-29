47593ef0243e4ae169cbb14f808ad093d265c20f
Utsawb Lamichhane <utsawblamichhane@gmail.com>
2024-04-29 15:44:50 -0500

    Email is supported!

--- front/app/api/send/route.ts
+++ front/app/api/send/route.ts
@@ -1,21 +1,18 @@
 import { EmailTemplate } from '@/components/email-template';
 import { Resend } from 'resend';
 import { Kitchen } from '@prisma/client';
-import * as React from 'react';
 
 const resend = new Resend(process.env.EMAIL_API);
 
 export async function POST(request: Request) {
     try {
         const kitchen: Kitchen[] = await request.json();
-        console.log("Got to email api");
-        console.log(kitchen);
         const subject = "Your Order: #" + kitchen[0].order_id + " is ready!";
         const { data, error } = await resend.emails.send({
             from: 'info@revs.utsawb.dev',
             to: kitchen[0].email as string,
             subject: subject,
-            react: EmailTemplate({ kitchen : kitchen }) as React.ReactElement,
+            react: EmailTemplate({ kitchen : kitchen })
         });
 
         if (error) {
