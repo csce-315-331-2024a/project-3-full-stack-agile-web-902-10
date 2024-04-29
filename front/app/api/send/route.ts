9e4c23a3345cb123eb4ca42c18c734e88fbef3bb
Utsawb Lamichhane <utsawblamichhane@gmail.com>
2024-04-29 14:41:50 -0500

    Fixed the prisma client error

--- front/app/api/send/route.ts
+++ front/app/api/send/route.ts
@@ -3,11 +3,13 @@ import { Resend } from 'resend';
 import { Kitchen } from '@prisma/client';
 import * as React from 'react';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.EMAIL_API);
 
 export async function POST(request: Request) {
     try {
         const kitchen: Kitchen[] = await request.json();
+        console.log("Got to email api");
+        console.log(kitchen);
         const subject = "Your Order: #" + kitchen[0].order_id + " is ready!";
         const { data, error } = await resend.emails.send({
             from: 'info@revs.utsawb.dev',
