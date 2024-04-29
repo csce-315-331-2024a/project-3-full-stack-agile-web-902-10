5928a5c4b198427c26fe22efc66b5ba33f50936b
Sean Lee <99452617+sean3-lee@users.noreply.github.com>
2024-04-28 19:08:13 -0500

    Update ManagerScreen.spec.ts
    added comments for clarity

--- front/tests/ManagerScreen.spec.ts
+++ front/tests/ManagerScreen.spec.ts
@@ -2,6 +2,8 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('http://localhost:3000/menu');
+
+  //login to site in order to access manager features
   await page.getByRole('button', { name: 'Settings' }).click();
   await page.getByRole('button', { name: 'Sign In' }).click();
   await page.getByLabel('Email or phone').fill('csce.313.team.10@gmail.com');
@@ -9,6 +11,8 @@ test('test', async ({ page }) => {
   await page.getByLabel('Enter your password').fill('5uO52^&Ea*l2');
   await page.getByRole('button', { name: 'Next' }).click();
   await page.getByRole('link', { name: 'Dashboard' }).click();
+
+  //start testing edit options
   await page.getByRole('button', { name: 'Edit Menu' }).click();
   await page.getByRole('img', { name: 'Corn Dog Value Meal' }).click();
   await page.getByRole('button', { name: 'Edit Ingredients' }).click();
@@ -16,14 +20,20 @@ test('test', async ({ page }) => {
   await page.getByRole('button', { name: 'Edit Ingredients' }).click();
   await page.locator('.flex > div > .flex > button').first().click();
   await page.getByRole('button', { name: 'Close' }).click();
+
+  //test the employee page
   await page.getByRole('button', { name: 'Employees' }).click();
   await page.getByRole('button', { name: 'Jonathan Moore Admin' }).first().click();
   await page.getByLabel('Admin: Jonathan Moore').click();
+
+  //test history data retrival 
   await page.getByRole('button', { name: 'Order History' }).click();
   await page.getByLabel('Go to next page').click();
   await page.getByLabel('Go to next page').click();
   await page.getByRole('button', { name: 'Login History' }).click();
   await page.getByLabel('Go to next page').dblclick();
+
+  //test manager trends page
   await page.getByRole('button', { name: 'Trends' }).click();
   await page.getByRole('button', { name: 'Product Usage Chart' }).click();
   await page.getByRole('button', { name: 'Sales Report' }).click();
@@ -34,6 +44,8 @@ test('test', async ({ page }) => {
   await page.getByRole('button', { name: 'What Sells Together' }).click();
   await page.getByRole('button', { name: 'What Sells Together(ℹ️)' }).click();
   await page.getByRole('button', { name: 'Back to Manager' }).click();
+
+  //test kitchen page
   await page.getByRole('button', { name: 'Kitchen' }).click();
   await page.getByRole('row', { name: '#48734 7/28/2024, 8:56:19 AM' }).getByRole('button').click();
   await page.getByRole('button', { name: 'Complete Order' }).click();
