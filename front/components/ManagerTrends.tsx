0c21793b5e9256c494fafbfe5fb5201700ad9f5b
anuccitelli2 <anuccitelli2@tamu.edu>
2024-05-01 15:17:05 -0500

    Date pickers now work for all trends
    Date pickers now work for all of the trends and update dynamically

--- front/components/ManagerTrends.tsx
+++ front/components/ManagerTrends.tsx
@@ -80,6 +80,16 @@ export default function ManagerTrends({ excessReportData, productUsageReportData
                 setProductUsage(data);
             });
         }
+        if (socket) {
+            socket.emit("rawQuery", auth, excessReportString(), (data: ExcessReportData[]) => {
+                setExcessReport(data);
+            });
+        }
+        if (socket) {
+            socket.emit("rawQuery", auth, whatSellsTogetherString(), (data: WhatSellsTogetherData[]) => {
+                setWhatSellsTogether(data);
+            });
+        }
     }, [beginDate, endDate]);
 
     const onButtonClick = (trend: string) => {
@@ -130,8 +140,54 @@ export default function ManagerTrends({ excessReportData, productUsageReportData
             TotalSales DESC;`
     }
 
+    function excessReportString() {
+        return `SELECT
+        "Ingredient".NAME AS Ingredient,
+        SUM("Ingredients_Menu".QUANTITY) AS TotalQuantityUsed,
+        "Ingredient".CATEGORY
+    FROM
+        "Order_Log"
+    JOIN
+        --string to array needs to change after transposition of order_menu.menu_item_id
+        "Menu_Item" ON "Menu_Item".ID = ANY(STRING_TO_ARRAY("Order_Log"."menu_items", ',')::INTEGER[])
+    JOIN
+        "Ingredients_Menu" ON "Menu_Item".ID = "Ingredients_Menu".MENU_ID
+    JOIN
+        "Ingredient" ON "Ingredients_Menu".INGREDIENTS_ID = "Ingredient".ID
+    WHERE
+        "Order_Log".time BETWEEN '`+ dateToString(beginDate ? beginDate : new Date(1999, 1, 1)) + ` 00:00:00' AND '`+ dateToString(new Date()) +` 23:59:59'
+    GROUP BY
+        "Ingredient".NAME, "Ingredient".CATEGORY, "Ingredient".STOCK
+    HAVING
+        SUM("Ingredients_Menu".QUANTITY) <  0.1* ("Ingredient".STOCK + SUM("Ingredients_Menu".QUANTITY))
+    ORDER BY
+        TotalQuantityUsed DESC;`
+    }
+
     const restockReportString = () => {
         return `SELECT * FROM "Ingredient" WHERE is_active = True AND STOCK < MIN_STOCK ORDER BY STOCK;`;
+    }
+
+    function whatSellsTogetherString() {
+        return `SELECT mi1.name AS item1_name, mi2.name AS item2_name, COUNT(*) AS frequency
+        FROM (
+            SELECT t1.item AS item1, t2.item AS item2
+            FROM (
+                SELECT id, UNNEST(STRING_TO_ARRAY(menu_items, ',')::INTEGER[]) AS item
+                FROM "Order_Log"
+                WHERE time >= '`+ dateToString(beginDate ? beginDate : new Date(1999, 1, 1)) + ` 00:00:00' AND time <= '`+ dateToString(endDate ? endDate : new Date()) +` 23:59:59'
+            ) AS t1
+            JOIN (
+                SELECT id, UNNEST(STRING_TO_ARRAY(menu_items, ',')::INTEGER[]) AS item
+                FROM "Order_Log"
+                WHERE time >= '`+ dateToString(beginDate ? beginDate : new Date(1999, 1, 1)) + ` 00:00:00' AND time <= '`+ dateToString(endDate ? endDate : new Date()) +` 23:59:59'
+            ) AS t2 ON t1.id = t2.id AND t1.item < t2.item
+        ) AS menu_pairs
+        JOIN "Menu_Item" AS mi1 ON menu_pairs.item1 = mi1.id
+        JOIN "Menu_Item" AS mi2 ON menu_pairs.item2 = mi2.id
+        GROUP BY item1_name, item2_name
+        ORDER BY frequency DESC
+        LIMIT 10;`
     }
 
     return (
